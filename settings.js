40a4f3f2c7d13ab21799525c947abf0a5b0023b5
MrchllAnuu.My.Id <95945703+MrchllAnuu@users.noreply.github.com>
2023-01-17 16:38:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Mrchllcloud' 
+global.apikey = ["Mrchll", "Mrchllcloud", "Mrchllhost"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
